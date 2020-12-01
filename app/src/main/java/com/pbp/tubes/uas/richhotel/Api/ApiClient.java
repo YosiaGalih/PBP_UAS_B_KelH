4634ebacd911528f1923cc794ccf97154ad1f571
DanuhadiWijaya <danuhadi.w@gmail.com>
2020-12-01 22:26:37 +0700

    API interface

--- app/src/main/java/com/pbp/tubes/uas/richhotel/Api/ApiClient.java
+++ app/src/main/java/com/pbp/tubes/uas/richhotel/Api/ApiClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    public static final String BASE_URL = "https://tubesluxuryhotel.000webhostapp.com/api/";
+    public static final String BASE_URL = "https://anyapibased.online/api/";
     public static Retrofit retrofit = null;
     public static Retrofit getClient(){
         if(retrofit == null) {
--- app/src/main/java/com/pbp/tubes/uas/richhotel/Api/ApiInterface.java
+++ app/src/main/java/com/pbp/tubes/uas/richhotel/Api/ApiInterface.java
@@ -5,50 +5,58 @@ import com.pbp.tubes.uas.richhotel.Response.TransaksiResponse;
 import com.pbp.tubes.uas.richhotel.Response.UserResponse;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Part;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
 
-    @GET("kamar")
-    Call<KamarResponse> getAllKamar(@Query("data")String data);
+    //--------------------------------------------API User--------------------------------------------//
+    @GET("user/{email}")
+    Call<KamarResponse> getUserByEmail(@Path("email")String email,
+                                       @Query("data")String data);
 
-    @GET("transaksi/{id_pemesan}")
-    Call<TransaksiResponse> gettTransaksiById(@Path("id_pemesan")String id,
-                                              @Query("data")String data);
-
-//    @GET("registrasi")
-//    Call<UserResponse> getUserById(@Path("id")String id,
-//                                   @Query("data")String data);
     @POST("registrasi")
     @FormUrlEncoded
     Call<UserResponse> createUser(@Field("name")String nama, @Field("age")String age,
                                   @Field("email")String email, @Field("password")String password);
 
-    @POST("registrasi/update/{id}")
+    @PUT("user/{id}")
     @FormUrlEncoded
-    Call<UserResponse> updateUser(@Field("nama")String nama, @Field("age")String age,
+    Call<UserResponse> updateUser(@Path("id")String id,
+                                  @Field("name")String nama, @Field("age")String age,
                                   @Field("email")String email, @Field("password")String password);
+
+    //--------------------------------------------API Kamar--------------------------------------------//
+
+    @GET("kamar")
+    Call<KamarResponse> getAllKamar(@Query("data")String data);
 
     @POST("kamar")
     @FormUrlEncoded
     Call<KamarResponse> createKamar(@Field("nama_kamar")String nama_kamar, @Field("kapasitas")String kapasitas,
                                     @Field("harga")String harga, @Field("imageURL")String image);
 
-    @POST("kamar_hotels/update/{id}")
+    @PUT("kamar/{id}")
     @FormUrlEncoded
-    Call<KamarResponse> updateKamar(@Field("nama_kamar")String nama_kamar, @Field("kapasitas")String kapasitas,
+    Call<KamarResponse> updateKamar(@Path("id")String id,
+                                    @Field("nama_kamar")String nama_kamar, @Field("kapasitas")String kapasitas,
                                     @Field("harga")String harga, @Field("imageURL")String image);
 
-    @POST("kamar_hotels/delete/{id}")
-    @FormUrlEncoded
-    Call<KamarResponse> deleteKamar(@Field("nama_kamar")String nama_kamar, @Field("kapasitas")String kapasitas,
-                                    @Field("harga")String harga, @Field("imageURL")String image);
+    @DELETE("kamar/{id}")
+    Call<KamarResponse> deleteKamar(@Path("id")String id);
+
+    //--------------------------------------------API Transaksi--------------------------------------------//
+
+    @GET("transaksi/{id_pemesan}")
+    Call<TransaksiResponse> gettTransaksiByIdPemesan(@Path("id_pemesan")String id_pemesan,
+                                              @Query("data")String data);
 
     @POST("transaksi")
     @FormUrlEncoded
@@ -56,16 +64,13 @@ public interface ApiInterface {
                                             @Field("alamat")String alamat, @Field("pilihan_kamar")String pilihan,
                                             @Field("tgl_check_in")String tglCheckIn, @Field("tgl_check_out")String tglCheckOut);
 
-    @POST("transaksis/update/{id_pemesan}")
+    @PUT("transaksi/{id}")
     @FormUrlEncoded
-    Call<TransaksiResponse> updateTransaksi(@Field("nama_pemesan")String nama_pemesan, @Field("id_pemesan")String id,
+    Call<TransaksiResponse> updateTransaksi(@Path("id")String id,
+                                            @Field("nama_pemesan")String nama_pemesan, @Field("id_pemesan")String id_pemesan,
                                             @Field("alamat")String alamat, @Field("pilihan_kamar")String pilihan,
                                             @Field("tgl_check_in")String tglCheckIn, @Field("tgl_check_out")String tglCheckOut);
 
-    @POST("transaksis/delete/{id_pemesan}")
-    @FormUrlEncoded
-    Call<TransaksiResponse> deleteTransaksi(@Field("nama_pemesan")String nama_pemesan, @Field("id_pemesan")String id,
-                                            @Field("alamat")String alamat, @Field("pilihan_kamar")String pilihan,
-                                            @Field("tgl_check_in")String tglCheckIn, @Field("tgl_check_out")String tglCheckOut);
-
+    @DELETE("transaksi/{id}")
+    Call<TransaksiResponse> deleteTransaksi(@Path("id")String id);
 }

1784faa312471ed198d9cff8f1e8c9974f8817a3
DanuhadiWijaya <danuhadi.w@gmail.com>
2020-12-07 01:14:41 +0700

    Instrumented Test

--- app/src/androidTest/java/com/pbp/tubes/uas/richhotel/UnitTest/CreateKamarTest.java
+++ app/src/androidTest/java/com/pbp/tubes/uas/richhotel/UnitTest/CreateKamarTest.java
@@ -20,9 +20,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.longClick;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
@@ -33,20 +33,21 @@ import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class InstrumentedTesting {
+public class CreateKamarTest {
 
     @Rule
     public ActivityTestRule<SplashScreen> mActivityTestRule = new ActivityTestRule<>(SplashScreen.class);
 
     @Test
-    public void instrumentedTesting() {
+    public void createKamarTest() throws InterruptedException {
+        Thread.sleep(6000);
         ViewInteraction materialButton = onView(
                 allOf(withId(R.id.getStarted), withText("GET STARTED"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
-                                        1),
-                                4),
+                                        0),
+                                3),
                         isDisplayed()));
         materialButton.perform(click());
 
@@ -55,8 +56,8 @@ public class InstrumentedTesting {
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
-                                        1),
-                                4),
+                                        0),
+                                2),
                         isDisplayed()));
         appCompatEditText.perform(replaceText("admin@gmail.com"), closeSoftKeyboard());
 
@@ -65,8 +66,8 @@ public class InstrumentedTesting {
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
-                                        1),
-                                4),
+                                        0),
+                                3),
                         isDisplayed()));
         appCompatEditText2.perform(replaceText("admin123"), closeSoftKeyboard());
 
@@ -108,17 +109,7 @@ public class InstrumentedTesting {
                                         0),
                                 0),
                         isDisplayed()));
-        textInputEditText.perform(click());
-
-        ViewInteraction textInputEditText2 = onView(
-                allOf(withId(R.id.tvNamaKamar),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.tvNamaKamar_layout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        textInputEditText2.perform(replaceText("baru"), closeSoftKeyboard());
+        textInputEditText.perform(replaceText("kamar"), closeSoftKeyboard());
 
         ViewInteraction materialButton4 = onView(
                 allOf(withId(R.id.btnAdd), withText("ADD"),
@@ -130,7 +121,7 @@ public class InstrumentedTesting {
                         isDisplayed()));
         materialButton4.perform(click());
 
-        ViewInteraction textInputEditText3 = onView(
+        ViewInteraction textInputEditText2 = onView(
                 allOf(withId(R.id.tvHarga),
                         childAtPosition(
                                 childAtPosition(
@@ -138,7 +129,7 @@ public class InstrumentedTesting {
                                         0),
                                 0),
                         isDisplayed()));
-        textInputEditText3.perform(replaceText("2500000"), closeSoftKeyboard());
+        textInputEditText2.perform(replaceText("1000"), closeSoftKeyboard());
 
         ViewInteraction materialButton5 = onView(
                 allOf(withId(R.id.btnAdd), withText("ADD"),
@@ -150,7 +141,7 @@ public class InstrumentedTesting {
                         isDisplayed()));
         materialButton5.perform(click());
 
-        ViewInteraction textInputEditText4 = onView(
+        ViewInteraction textInputEditText3 = onView(
                 allOf(withId(R.id.tvKapasitas),
                         childAtPosition(
                                 childAtPosition(
@@ -158,7 +149,7 @@ public class InstrumentedTesting {
                                         0),
                                 0),
                         isDisplayed()));
-        textInputEditText4.perform(replaceText("3"), closeSoftKeyboard());
+        textInputEditText3.perform(replaceText("10000"), closeSoftKeyboard());
 
         ViewInteraction materialButton6 = onView(
                 allOf(withId(R.id.btnAdd), withText("ADD"),
@@ -170,7 +161,7 @@ public class InstrumentedTesting {
                         isDisplayed()));
         materialButton6.perform(click());
 
-        ViewInteraction textInputEditText5 = onView(
+        ViewInteraction textInputEditText4 = onView(
                 allOf(withId(R.id.tvGambar),
                         childAtPosition(
                                 childAtPosition(
@@ -178,57 +169,27 @@ public class InstrumentedTesting {
                                         0),
                                 0),
                         isDisplayed()));
-        textInputEditText5.perform(longClick());
+        textInputEditText4.perform(replaceText("none"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText5 = onView(
+                allOf(withId(R.id.tvNamaKamar), withText("kamar"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tvNamaKamar_layout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText5.perform(replaceText(""));
 
         ViewInteraction textInputEditText6 = onView(
-                allOf(withId(R.id.tvGambar),
+                allOf(withId(R.id.tvNamaKamar),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(R.id.tvGambar_layout),
+                                        withId(R.id.tvNamaKamar_layout),
                                         0),
                                 0),
                         isDisplayed()));
-        textInputEditText6.perform(click());
-
-        ViewInteraction textInputEditText7 = onView(
-                allOf(withId(R.id.tvGambar),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.tvGambar_layout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        textInputEditText7.perform(longClick());
-
-        ViewInteraction textInputEditText8 = onView(
-                allOf(withId(R.id.tvGambar),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.tvGambar_layout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        textInputEditText8.perform(replaceText("https://www.pegipegi.com/travel/wp-content/uploads/2016/06/hotel-mewah-di-yogyakarta.jpg"), closeSoftKeyboard());
-
-        ViewInteraction textInputEditText9 = onView(
-                allOf(withId(R.id.tvKapasitas), withText("3"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.tvKapasitas_layout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        textInputEditText9.perform(replaceText(""));
-
-        ViewInteraction textInputEditText10 = onView(
-                allOf(withId(R.id.tvKapasitas),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.tvKapasitas_layout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        textInputEditText10.perform(closeSoftKeyboard());
+        textInputEditText6.perform(closeSoftKeyboard());
 
         ViewInteraction materialButton7 = onView(
                 allOf(withId(R.id.btnAdd), withText("ADD"),
@@ -240,27 +201,37 @@ public class InstrumentedTesting {
                         isDisplayed()));
         materialButton7.perform(click());
 
-        ViewInteraction textInputEditText11 = onView(
-                allOf(withId(R.id.tvKapasitas),
+        ViewInteraction textInputEditText7 = onView(
+                allOf(withId(R.id.tvNamaKamar),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(R.id.tvKapasitas_layout),
+                                        withId(R.id.tvNamaKamar_layout),
                                         0),
                                 0),
                         isDisplayed()));
-        textInputEditText11.perform(replaceText("5"), closeSoftKeyboard());
+        textInputEditText7.perform(click());
 
-        ViewInteraction textInputEditText12 = onView(
-                allOf(withId(R.id.tvHarga), withText("2500000"),
+        ViewInteraction textInputEditText8 = onView(
+                allOf(withId(R.id.tvNamaKamar),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tvNamaKamar_layout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText8.perform(replaceText("kamar"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText9 = onView(
+                allOf(withId(R.id.tvHarga), withText("1000"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(R.id.tvHarga_layout),
                                         0),
                                 0),
                         isDisplayed()));
-        textInputEditText12.perform(replaceText(""));
+        textInputEditText9.perform(replaceText(""));
 
-        ViewInteraction textInputEditText13 = onView(
+        ViewInteraction textInputEditText10 = onView(
                 allOf(withId(R.id.tvHarga),
                         childAtPosition(
                                 childAtPosition(
@@ -268,7 +239,7 @@ public class InstrumentedTesting {
                                         0),
                                 0),
                         isDisplayed()));
-        textInputEditText13.perform(closeSoftKeyboard());
+        textInputEditText10.perform(closeSoftKeyboard());
 
         ViewInteraction materialButton8 = onView(
                 allOf(withId(R.id.btnAdd), withText("ADD"),
@@ -280,7 +251,7 @@ public class InstrumentedTesting {
                         isDisplayed()));
         materialButton8.perform(click());
 
-        ViewInteraction textInputEditText14 = onView(
+        ViewInteraction textInputEditText11 = onView(
                 allOf(withId(R.id.tvHarga),
                         childAtPosition(
                                 childAtPosition(
@@ -288,7 +259,27 @@ public class InstrumentedTesting {
                                         0),
                                 0),
                         isDisplayed()));
-        textInputEditText14.perform(replaceText("370000"), closeSoftKeyboard());
+        textInputEditText11.perform(replaceText("50000"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText12 = onView(
+                allOf(withId(R.id.tvKapasitas), withText("10000"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tvKapasitas_layout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText12.perform(replaceText(""));
+
+        ViewInteraction textInputEditText13 = onView(
+                allOf(withId(R.id.tvKapasitas),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tvKapasitas_layout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText13.perform(closeSoftKeyboard());
 
         ViewInteraction materialButton9 = onView(
                 allOf(withId(R.id.btnAdd), withText("ADD"),
@@ -299,6 +290,38 @@ public class InstrumentedTesting {
                                 5),
                         isDisplayed()));
         materialButton9.perform(click());
+
+        ViewInteraction textInputEditText14 = onView(
+                allOf(withId(R.id.tvKapasitas),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tvKapasitas_layout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText14.perform(click());
+
+        ViewInteraction textInputEditText15 = onView(
+                allOf(withId(R.id.tvKapasitas),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tvKapasitas_layout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText15.perform(replaceText("4"), closeSoftKeyboard());
+
+        ViewInteraction materialButton10 = onView(
+                allOf(withId(R.id.btnAdd), withText("ADD"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                5),
+                        isDisplayed()));
+        materialButton10.perform(click());
+
+        pressBack();
     }
 
     private static Matcher<View> childAtPosition(
